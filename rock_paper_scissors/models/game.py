67d2631b2d4b7d02fa9ec5b8fa8ca58e6b6efe97
Alastair Smith <j.alastair.smith@outlook.com>
2021-05-29 15:51:25 +0100

    passes test for  rules dictionary and decide result method on game class

--- rock_paper_scissors/models/game.py
+++ rock_paper_scissors/models/game.py
@@ -1,8 +1,18 @@
 class Game:
 
-    def __init__(self, rules):
+    def __init__(self):
         self.rules = {
             "rock" : "scissors",
             "paper": "rock",
             "scissors": "paper"
         }
+
+    # def get_preferred_option(task1, task2):
+    # if task2.description in task1.lookup[task1.description]:
+    #     return task1.description
+    # return task2.description
+
+    def decide_result(self, player1_choice, player2_choice):
+        if player2_choice in self.rules[player1_choice]:
+            return player1_choice
+        return player2_choice
--- rock_paper_scissors/tests/game_test.py
+++ rock_paper_scissors/tests/game_test.py
@@ -2,4 +2,25 @@ import unittest
 from models.game import Game
 
 class TestGame(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.game = Game()
+
+    def test_rules__rock_beats_scissors(self):
+        self.assertEqual("scissors", self.game.rules["rock"])
+
+    def test_rules__paper_beats_rock(self):
+        self.assertEqual("rock", self.game.rules["paper"])        
+
+    def test_rules__scissors_beats_paper(self):
+        self.assertEqual("paper", self.game.rules["scissors"])
+
+
+    def test_method_decide_result_returns_correct_result___rock_wins(self):
+        self.assertEqual("rock", self.game.decide_result("rock", "scissors"))
+
+    def test_method_decide_result_returns_correct_result___draw(self):
+        self.assertEqual("rock", self.game.decide_result("rock", "rock"))
+
+    def test_method_decide_result_returns_correct_result___rock_loses(self):
+        self.assertEqual("paper", self.game.decide_result("rock", "paper"))
