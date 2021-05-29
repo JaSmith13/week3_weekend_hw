3287371560312dca4d0d4357327bf7956194c6d7
Alastair Smith <j.alastair.smith@outlook.com>
2021-05-29 15:31:37 +0100

    passes attribute check tests for player class

--- rock_paper_scissors/models/player.py
+++ rock_paper_scissors/models/player.py
@@ -3,4 +3,4 @@ class Player:
     def __init__(self, player_name, player_choice):
         self.name = player_name
         self.choice = player_choice
-        pass
+        
--- rock_paper_scissors/tests/player_test.py
+++ rock_paper_scissors/tests/player_test.py
@@ -2,4 +2,13 @@ import unittest
 from models.player import Player
 
 class TestPlayer(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.player1 = Player('Scott', 'scissors')
+        self.player2 = Player('Regina', 'rock')
+
+    def test_player_has_name(self):
+        self.assertEqual('Scott', self.player1.name)
+
+    def test_player_has_choice(self):
+        self.assertEqual('scissors', self.player1.choice)
